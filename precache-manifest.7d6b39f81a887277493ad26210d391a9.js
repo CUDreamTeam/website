3fbafbf4b3d21c1844ca804f35c1144aedd8af74
itsMichal <michaljbodzianowski@gmail.com>
2020-04-13 00:45:13 -0600

    Updates

---
+++ precache-manifest.7d6b39f81a887277493ad26210d391a9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ab70bc0100a191c9e2134095fad0a6",
+    "url": "/CUDreamTeam/website/index.html"
+  },
+  {
+    "revision": "96c13121a21b26961098",
+    "url": "/CUDreamTeam/website/static/css/main.b60eba9f.chunk.css"
+  },
+  {
+    "revision": "c01b91837b139d768f78",
+    "url": "/CUDreamTeam/website/static/js/2.abbc76ba.chunk.js"
+  },
+  {
+    "revision": "96c13121a21b26961098",
+    "url": "/CUDreamTeam/website/static/js/main.4f51f72b.chunk.js"
+  },
+  {
+    "revision": "e89ec8b92d9125847f7a",
+    "url": "/CUDreamTeam/website/static/js/runtime~main.33ae3cab.js"
+  },
+  {
+    "revision": "baa262cc8769a7cfde01ce5671118c84",
+    "url": "/CUDreamTeam/website/static/media/Deutsch.baa262cc.ttf"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/CUDreamTeam/website/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/CUDreamTeam/website/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/CUDreamTeam/website/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/CUDreamTeam/website/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/CUDreamTeam/website/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/CUDreamTeam/website/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/CUDreamTeam/website/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/CUDreamTeam/website/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/CUDreamTeam/website/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/CUDreamTeam/website/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/CUDreamTeam/website/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/CUDreamTeam/website/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ba1d105c07268645859349edb94462c2",
+    "url": "/CUDreamTeam/website/static/media/testlogo.ba1d105c.png"
+  }
+]);
